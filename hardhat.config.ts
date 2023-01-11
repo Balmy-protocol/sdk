ae42ba9b44e8e576a6c5d4c6880c67d08a15a8f6
Sam Bugs <sambugs@protonmail.com>
2023-01-11 05:07:20 -0300

    test: split quote test

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,8 @@ import { HardhatUserConfig } from 'hardhat/types';
 import '@nomiclabs/hardhat-ethers';
 import '@nomicfoundation/hardhat-chai-matchers';
 import 'tsconfig-paths/register';
+import dotenv from 'dotenv'
+dotenv.config()
 
 const config: HardhatUserConfig = {
   defaultNetwork: 'hardhat',
