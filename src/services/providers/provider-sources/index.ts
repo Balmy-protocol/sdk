4f5424a0be1f0e2b93fe88f6c443780a34f8fa7c
Fiboape <94012134+FiboApe@users.noreply.github.com>
2023-01-16 18:40:25 -0300

    chore(*): packagetize sdk (#17)
    * chore(*): packagetize sdk
    
    * fixup! chore(*): packagetize sdk

---
+++ src/services/providers/provider-sources/index.ts
@@ -0,0 +1,11 @@
+import { FallbackSource } from './fallback-provider ';
+import { PrioritizedProviderSourceCombinator } from './prioritized-provider-source-combinator';
+import { PublicProvidersSource } from './public-providers';
+import { SingleProviderSource } from './single-provider';
+
+export default {
+  FallbackSource,
+  PrioritizedProviderSourceCombinator,
+  PublicProvidersSource,
+  SingleProviderSource,
+};
