aec150054fb4924081bd1fa872ac5e782727c663
Sam Bugs <sambugs@protonmail.com>
2023-01-24 16:29:40 -0300

    refactor: add consistency to sdk builders

--- src/sdk/sdk-builder.ts
+++ src/sdk/sdk-builder.ts
@@ -3,18 +3,18 @@ import { BuildFetchParams, buildFetchService } from './builders/fetch-builder';
 import { BuildProviderParams, buildProviderSource } from './builders/provider-source-builder';
 import { buildGasService, BuildGasParams } from './builders/gas-builder';
 import { buildMulticallService } from './builders/multicall-builder';
-import { BuildTokenParams, buildTokenService, CalculateTokenFromSources } from './builders/token-builder';
+import { BuildTokenParams, buildTokenService, CalculateTokenFromSourceParams } from './builders/token-builder';
 import { BuildQuoteParams, buildQuoteService } from './builders/quote-builder';
 import { ISDK } from './types';
 
 export function buildSDK<Params extends BuildParams<CustomConfig>, CustomConfig extends Partial<AllSourcesConfig> = {}>(
   params?: Params
-): ISDK<SourcesBasedOnConfig<CustomConfig>, CalculateTokenFromSources<Params['tokens']>> {
+): ISDK<SourcesBasedOnConfig<CustomConfig>, CalculateTokenFromSourceParams<Params['tokens']>> {
   const fetchService = buildFetchService(params?.fetch);
   const providerSource = buildProviderSource(params?.provider);
   const multicallService = buildMulticallService(providerSource);
   const gasService = buildGasService(params?.gas, fetchService, providerSource, multicallService);
-  const tokenService = buildTokenService(params?.tokens, fetchService, multicallService);
+  const tokenService = buildTokenService<Params['tokens']>(params?.tokens, fetchService, multicallService);
   const quoteService = buildQuoteService(params?.quotes, fetchService, gasService, tokenService);
 
   return {
