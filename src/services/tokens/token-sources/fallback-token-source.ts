67ed70c8589bb7e36bafeacfe26091be4bd280f1
Sam Bugs <sambugs@protonmail.com>
2023-01-30 15:15:41 -0300

    refactor: improve fallback token source

--- src/services/tokens/token-sources/fallback-token-source.ts
+++ src/services/tokens/token-sources/fallback-token-source.ts
@@ -12,34 +12,116 @@ export class FallbackTokenSource<Sources extends ITokenSource<BaseToken>[] | []>
     return chainsUnion(this.sources.map((source) => source.supportedChains()));
   }
 
-  async getTokens(
+  getTokens(
     addresses: Record<ChainId, TokenAddress[]>,
     context?: { timeout?: TimeString }
   ): Promise<Record<ChainId, Record<TokenAddress, MergeTokenTokensFromSources<Sources>>>> {
-    const result: Record<ChainId, Record<TokenAddress, MergeTokenTokensFromSources<Sources>>> = Object.fromEntries(
-      Object.keys(addresses).map((chainId) => [chainId, {}])
-    );
+    return new Promise<Record<ChainId, Record<TokenAddress, MergeTokenTokensFromSources<Sources>>>>((resolve, reject) => {
+      const result: Record<ChainId, Record<TokenAddress, MergeTokenTokensFromSources<Sources>>> = {};
+      const propertiesCounter = this.buildPropertiesCounter(addresses);
 
-    // TODO: Make it smarter. The idea would be that if a source has finished and returned all tokens, we can resolve the promise.
-    // Even if there are others that still haven't finished, but don't have any new properties. If there are unfinished sources with
-    // new properties, or there are some missing tokens, we would still have to wait
-    const promises = this.sources.map(async (source) => {
-      const addressesForSource = getAddressesForSource(source, addresses);
-      const sourceResult = await timeoutPromise(source.getTokens(addressesForSource), context?.timeout, { reduceBy: '100' });
-      for (const chainId in sourceResult) {
-        for (const token of Object.values(sourceResult[chainId])) {
-          const previousToken = result[chainId][token.address];
-          result[chainId][token.address] = { ...previousToken, ...token };
+      let sourcesLeftToFulfil = this.sources.length;
+      let successfulRequests = 0;
+
+      const handleFulfil = (source: ITokenSource) => {
+        this.updatePropertiesCounterWhenSourceFulfilled(propertiesCounter, source, addresses);
+
+        if (--sourcesLeftToFulfil === 0 || Object.keys(propertiesCounter).length === 0) {
+          if (successfulRequests > 0) {
+            resolve(result);
+          } else {
+            reject(new Error('Could not find tokens for the given addresses'));
+          }
         }
-      }
-    });
+      };
 
-    await Promise.allSettled(promises);
-    return result;
+      this.sources.forEach(async (source) => {
+        const addressesForSource = getAddressesForSource(source, addresses);
+        if (Object.keys(addressesForSource).length === 0) {
+          // If there is nothing to query for this source, exit
+          handleFulfil(source);
+          return;
+        }
+
+        timeoutPromise(source.getTokens(addressesForSource), context?.timeout, { reduceBy: '100' })
+          .then((sourceResult) => {
+            successfulRequests++;
+            for (const [chainIdString, tokenRecord] of Object.entries(sourceResult)) {
+              const chainId = parseInt(chainIdString);
+              const tokens = Object.values(tokenRecord);
+              if (!(chainId in result) && tokens.length > 0) result[chainId] = {};
+
+              for (const token of tokens) {
+                // Add to result
+                result[chainId][token.address] = { ...result[chainId][token.address], ...token };
+
+                // Remove from counter
+                for (const tokenProperty in token) {
+                  const property = tokenProperty as keyof MergeTokenTokensFromSources<Sources>;
+                  delete propertiesCounter[chainId][token.address][property];
+                }
+              }
+            }
+          })
+          .finally(() => handleFulfil(source));
+      });
+    });
   }
 
   tokenProperties(): PropertiesRecord<MergeTokenTokensFromSources<Sources>> {
     return combineTokenProperties(this.sources);
+  }
+
+  private buildPropertiesCounter(addresses: Record<ChainId, TokenAddress[]>) {
+    const propertiesCounter: Record<ChainId, Record<TokenAddress, Record<keyof MergeTokenTokensFromSources<Sources>, number>>> = {};
+    for (const chainId in addresses) {
+      propertiesCounter[chainId];
+      const counter: Record<keyof MergeTokenTokensFromSources<Sources>, number> = {} as any;
+      for (const source of this.sources) {
+        if (source.supportedChains().includes(parseInt(chainId))) {
+          for (const tokenProperty in source.tokenProperties()) {
+            const property = tokenProperty as keyof MergeTokenTokensFromSources<Sources>;
+            counter[property] = (counter[property] ?? 0) + 1;
+          }
+        }
+      }
+      propertiesCounter[chainId] = {};
+      for (const tokenAddress of addresses[chainId]) {
+        propertiesCounter[chainId][tokenAddress] = counter;
+      }
+    }
+    return propertiesCounter;
+  }
+
+  private updatePropertiesCounterWhenSourceFulfilled(
+    propertiesCounter: Record<ChainId, Record<TokenAddress, Record<keyof MergeTokenTokensFromSources<Sources>, number>>>,
+    source: ITokenSource<BaseToken>,
+    addresses: Record<ChainId, TokenAddress[]>
+  ) {
+    const tokenProperties = source.tokenProperties();
+    const addressesForSource = getAddressesForSource(source, addresses);
+    for (const [chainIdString, addresses] of Object.entries(addressesForSource)) {
+      const chainId = parseInt(chainIdString);
+      for (const address of addresses) {
+        for (const tokenProperty in tokenProperties) {
+          const property = tokenProperty as keyof MergeTokenTokensFromSources<Sources>;
+          const counter = propertiesCounter[chainId][address][property];
+          if (counter !== undefined) {
+            if (counter === 1) {
+              delete propertiesCounter[chainId][address][property];
+            } else {
+              propertiesCounter[chainId][address][property] = counter - 1;
+            }
+          }
+        }
+        if (Object.keys(propertiesCounter[chainId][address]).length === 0) {
+          delete propertiesCounter[chainId][address];
+        }
+      }
+      if (Object.keys(propertiesCounter[chainId]).length === 0) {
+        delete propertiesCounter[chainId];
+      }
+    }
   }
 }
 
---
+++ test/unit/services/tokens/token-sources/fallback-token-source.spec.ts
@@ -0,0 +1,246 @@
+import { expect } from 'chai';
+import { Chains } from '@chains';
+import { then, when } from '@test-utils/bdd';
+import { BaseToken, ITokenSource, MergeTokenTokensFromSources, PropertiesRecord } from '@services/tokens/types';
+import { Chain, ChainId, TimeString, TokenAddress } from '@types';
+import { FallbackTokenSource } from '@services/tokens/token-sources/fallback-token-source';
+import ms from 'ms';
+
+const TOKEN_A = {
+  address: '0x0000000000000000000000000000000000000001',
+  symbol: 'TKNA',
+  decimals: 18,
+};
+
+const TOKEN_A_WITH_EXTRA = {
+  ...TOKEN_A,
+  extra: 'extra',
+};
+
+const TOKEN_B = {
+  address: '0x0000000000000000000000000000000000000002',
+  symbol: 'TKNB',
+  decimals: 18,
+};
+
+describe('Fallback Token Source', () => {
+  when('source with all properties resolves', () => {
+    then('fallback resolves even if other sources are pending', async () => {
+      const { source: source1 } = source(Chains.POLYGON);
+      const { source: source2, promise: source2Promise } = sourceWithExtra(Chains.POLYGON);
+      const result = { [Chains.POLYGON.chainId]: { [TOKEN_A.address]: TOKEN_A_WITH_EXTRA } };
+
+      const promise = getTokensFromSources({ [Chains.POLYGON.chainId]: [TOKEN_A.address] }, source1, source2);
+      expect(promise.status).to.equal('pending');
+      source2Promise.resolve(result);
+      await sleep('10');
+      expect(promise.status).to.equal('resolved');
+      expect(await promise.result).to.deep.equal(result);
+    });
+  });
+
+  when('the only source with all properties resolves but extra properties are missing', () => {
+    then('fallback still resolves', async () => {
+      const { source: source1 } = source(Chains.POLYGON);
+      const { source: source2, promise: source2Promise } = sourceWithExtra(Chains.POLYGON);
+      const result = { [Chains.POLYGON.chainId]: { [TOKEN_A.address]: TOKEN_A } };
+
+      const promise = getTokensFromSources({ [Chains.POLYGON.chainId]: [TOKEN_A.address] }, source1, source2);
+      expect(promise.status).to.equal('pending');
+      source2Promise.resolve(result);
+      await sleep('10');
+      expect(promise.status).to.equal('resolved');
+      expect(await promise.result).to.deep.equal(result);
+    });
+  });
+
+  when('one source with all properties resolves but extra properties are missing', () => {
+    then('fallback waits for other sources with all properties', async () => {
+      const { source: source1, promise: source1Promise } = sourceWithExtra(Chains.POLYGON);
+      const { source: source2, promise: source2Promise } = sourceWithExtra(Chains.POLYGON);
+      const result = { [Chains.POLYGON.chainId]: { [TOKEN_A.address]: TOKEN_A } };
+      const resultWithProperties = { [Chains.POLYGON.chainId]: { [TOKEN_A.address]: TOKEN_A_WITH_EXTRA } };
+
+      const promise = getTokensFromSources({ [Chains.POLYGON.chainId]: [TOKEN_A.address] }, source1, source2);
+      expect(promise.status).to.equal('pending');
+      source1Promise.resolve(result);
+      await sleep('10');
+      expect(promise.status).to.equal('pending');
+      source2Promise.resolve(resultWithProperties);
+      await sleep('10');
+      expect(promise.status).to.equal('resolved');
+      expect(await promise.result).to.deep.equal(resultWithProperties);
+    });
+  });
+
+  when('all sources with extra properties resolved without these properties', () => {
+    then('fallback still resolves', async () => {
+      const { source: source1, promise: source1Promise } = sourceWithExtra(Chains.POLYGON);
+      const { source: source2, promise: source2Promise } = sourceWithExtra(Chains.POLYGON);
+      const result = { [Chains.POLYGON.chainId]: { [TOKEN_A.address]: TOKEN_A } };
+
+      const promise = getTokensFromSources({ [Chains.POLYGON.chainId]: [TOKEN_A.address] }, source1, source2);
+      expect(promise.status).to.equal('pending');
+      source1Promise.resolve(result);
+      await sleep('10');
+      expect(promise.status).to.equal('pending');
+      source2Promise.resolve(result);
+      await sleep('10');
+      expect(promise.status).to.equal('resolved');
+      expect(await promise.result).to.deep.equal(result);
+    });
+  });
+
+  when('the only source with all properties resolves but there are some missing tokens', () => {
+    then('fallback waits for other source without extra properties', async () => {
+      const { source: source1, promise: source1Promise } = sourceWithExtra(Chains.POLYGON);
+      const { source: source2, promise: source2Promise } = source(Chains.POLYGON);
+      const resultWithA = { [Chains.POLYGON.chainId]: { [TOKEN_A.address]: TOKEN_A } };
+      const resultWithB = { [Chains.POLYGON.chainId]: { [TOKEN_A.address]: TOKEN_A, [TOKEN_B.address]: TOKEN_B } };
+
+      const promise = getTokensFromSources({ [Chains.POLYGON.chainId]: [TOKEN_A.address, TOKEN_B.address] }, source1, source2);
+      expect(promise.status).to.equal('pending');
+      source1Promise.resolve(resultWithA);
+      await sleep('10');
+      expect(promise.status).to.equal('pending');
+      source2Promise.resolve(resultWithB);
+      await sleep('10');
+      expect(promise.status).to.equal('resolved');
+      expect(await promise.result).to.deep.equal(resultWithB);
+    });
+  });
+
+  when('the source with all properties does not support the chain', () => {
+    then('fallback resolves with the source without properties', async () => {
+      const { source: source1 } = sourceWithExtra(Chains.ETHEREUM);
+      const { source: source2, promise: source2Promise } = source(Chains.POLYGON);
+      const result = { [Chains.POLYGON.chainId]: { [TOKEN_A.address]: TOKEN_A } };
+
+      const promise = getTokensFromSources({ [Chains.POLYGON.chainId]: [TOKEN_A.address] }, source1, source2);
+      expect(promise.status).to.equal('pending');
+      source2Promise.resolve(result);
+      await sleep('10');
+      expect(promise.status).to.equal('resolved');
+      expect(await promise.result).to.deep.equal(result);
+    });
+  });
+
+  when('the source without all properties resolves', () => {
+    then('fallback waits for the one with all the properties to resolve', async () => {
+      const { source: source1, promise: source1Promise } = sourceWithExtra(Chains.POLYGON);
+      const { source: source2, promise: source2Promise } = source(Chains.POLYGON);
+      const result = { [Chains.POLYGON.chainId]: { [TOKEN_A.address]: TOKEN_A } };
+
+      const promise = getTokensFromSources({ [Chains.POLYGON.chainId]: [TOKEN_A.address] }, source1, source2);
+      expect(promise.status).to.equal('pending');
+      source2Promise.resolve(result);
+      await sleep('10');
+      expect(promise.status).to.equal('pending');
+      source1Promise.resolve(result);
+      await sleep('10');
+      expect(promise.status).to.equal('resolved');
+      expect(await promise.result).to.deep.equal(result);
+    });
+  });
+
+  when('the source without all properties resolves', () => {
+    then('fallback waits for the one with all the properties to reject', async () => {
+      const { source: source1, promise: source1Promise } = sourceWithExtra(Chains.POLYGON);
+      const { source: source2, promise: source2Promise } = source(Chains.POLYGON);
+      const result = { [Chains.POLYGON.chainId]: { [TOKEN_A.address]: TOKEN_A } };
+
+      const promise = getTokensFromSources({ [Chains.POLYGON.chainId]: [TOKEN_A.address] }, source1, source2);
+      expect(promise.status).to.equal('pending');
+      source2Promise.resolve(result);
+      await sleep('10');
+      expect(promise.status).to.equal('pending');
+      source1Promise.reject();
+      await sleep('10');
+      expect(promise.status).to.equal('resolved');
+      expect(await promise.result).to.deep.equal(result);
+    });
+  });
+
+  when('all sources fail', () => {
+    then('fallback rejects', async () => {
+      const { source: source1, promise: source1Promise } = sourceWithExtra(Chains.POLYGON);
+      const { source: source2, promise: source2Promise } = source(Chains.POLYGON);
+      const result = { [Chains.POLYGON.chainId]: { [TOKEN_A.address]: TOKEN_A } };
+
+      const promise = getTokensFromSources({ [Chains.POLYGON.chainId]: [TOKEN_A.address] }, source1, source2);
+      expect(promise.status).to.equal('pending');
+      source1Promise.reject(result);
+      await sleep('10');
+      expect(promise.status).to.equal('pending');
+      source2Promise.reject();
+      await sleep('10');
+      expect(promise.status).to.equal('rejected');
+    });
+  });
+
+  function source(...chains: Chain[]) {
+    return buildSource<BaseToken>({
+      chains,
+    });
+  }
+
+  function sourceWithExtra(...chains: Chain[]) {
+    return buildSource<TokenWithExtra>({
+      chains,
+      properties: {
+        extra: 'optional',
+      },
+    });
+  }
+
+  function getTokensFromSources<Sources extends ITokenSource<BaseToken>[]>(addresses: Record<ChainId, TokenAddress[]>, ...sources: Sources) {
+    const result = new FallbackTokenSource(sources).getTokens(addresses);
+    const promiseWithState: PromiseWithState<Record<ChainId, Record<TokenAddress, MergeTokenTokensFromSources<Sources>>>> = {
+      result,
+      status: 'pending',
+    };
+    result.then(() => (promiseWithState.status = 'resolved')).catch(() => (promiseWithState.status = 'rejected'));
+    return promiseWithState;
+  }
+
+  function promise<T>(): PromiseWithTriggers<T> {
+    let resolveExternal: (value: T) => void, rejectExternal: (error?: any) => void;
+    const promise = new Promise<T>((resolve, reject) => {
+      resolveExternal = resolve;
+      rejectExternal = reject;
+    });
+    // @ts-ignore
+    return Object.assign(promise, { resolve: resolveExternal, reject: rejectExternal });
+  }
+
+  function buildSource<Token extends BaseToken>({
+    chains,
+    properties,
+  }: {
+    chains: Chain[];
+    properties?: Omit<PropertiesRecord<Token>, keyof BaseToken>;
+  }): { source: ITokenSource<Token>; promise: PromiseWithTriggers<Record<ChainId, Record<TokenAddress, Token>>> } {
+    const sourcePromise = promise<Record<ChainId, Record<TokenAddress, Token>>>();
+    const source: ITokenSource<Token> = {
+      supportedChains: () => chains.map(({ chainId }) => chainId),
+      getTokens: () => sourcePromise,
+      tokenProperties: () => {
+        return {
+          address: 'present',
+          decimals: 'present',
+          symbol: 'present',
+          ...properties,
+        } as PropertiesRecord<Token>;
+      },
+    };
+    return { source, promise: sourcePromise };
+  }
+});
+
+function sleep(time: TimeString) {
+  return new Promise((resolve) => setTimeout(resolve, ms(time)));
+}
+
+type TokenWithExtra = BaseToken & { extra?: string };
+type PromiseWithTriggers<T> = Promise<T> & { resolve: (value: T) => void; reject: (error?: any) => void };
+type PromiseWithState<T> = { status: 'pending' | 'resolved' | 'rejected'; result: Promise<T> };
