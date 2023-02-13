f6c92ee852259797236c1f04e4a8429510255ebd
Ged <0xged@protonmail.com>
2023-02-13 15:26:05 -0300

    fix: adds OKC as supported chain for Rango

--- src/services/quotes/quote-sources/rango.ts
+++ src/services/quotes/quote-sources/rango.ts
@@ -14,6 +14,7 @@ const SUPPORTED_CHAINS: Record<ChainId, string> = {
   [Chains.FANTOM.chainId]: 'FANTOM',
   [Chains.ARBITRUM.chainId]: 'ARBITRUM',
   [Chains.CELO.chainId]: 'CELO',
+  [Chains.OKC.chainId]: 'OKC',
   [Chains.MOONRIVER.chainId]: 'MOONRIVER',
   [Chains.MOONBEAM.chainId]: 'MOONBEAM',
   [Chains.OPTIMISM.chainId]: 'OPTIMISM',
