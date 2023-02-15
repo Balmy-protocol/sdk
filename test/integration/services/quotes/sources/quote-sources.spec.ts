56e1116f790ed7334cb001f6be2cd1ea53750447
Ged <0xged@protonmail.com>
2023-02-15 00:26:08 -0300

    test: rollback to valid integration quote

--- test/integration/services/quotes/sources/quote-sources.spec.ts
+++ test/integration/services/quotes/sources/quote-sources.spec.ts
@@ -34,7 +34,7 @@ import { buildSDK } from '@builder';
 // This is meant to be used for local testing. On the CI, we will do something different
 const RUN_FOR: { source: string; chains: Chain[] | 'all' } = {
   source: 'rango',
-  chains: [Chains.EVMOS],
+  chains: [Chains.ARBITRUM],
 };
 const ROUNDING_ISSUES: SourceId[] = ['rango'];
 
