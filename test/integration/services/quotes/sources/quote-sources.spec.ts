4e60103d7b3aa8de959f144e56fb2dac315d611b
Ged <0xged@protonmail.com>
2023-02-15 14:27:07 -0300

    chore: rollsback some of the sovryn changes

--- test/integration/services/quotes/sources/quote-sources.spec.ts
+++ test/integration/services/quotes/sources/quote-sources.spec.ts
@@ -39,7 +39,7 @@ const RUN_FOR: { source: string; chains: Chain[] | 'all' } = {
   source: 'rango',
   chains: [Chains.ARBITRUM],
 };
-const ROUNDING_ISSUES: SourceId[] = [];
+const ROUNDING_ISSUES: SourceId[] = ['rango'];
 
 // Since trading tests can be a little bit flaky, we want to re-test before failing
 jest.retryTimes(3);
