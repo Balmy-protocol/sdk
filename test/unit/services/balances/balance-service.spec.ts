e0eca11c367e1c9ba41c3fc033be946d4bae7113
Sam Bugs <sambugs@protonmail.com>
2023-02-20 10:38:02 -0300

    fix: getBalancesForTokens on balance service

---
+++ test/unit/services/balances/balance-service.spec.ts
@@ -0,0 +1,77 @@
+import { expect } from 'chai';
+import { Chains } from '@chains';
+import { Address, AmountOfToken, ChainId, TimeString, TokenAddress } from '@types';
+import { BalanceQueriesSupport, IBalanceService, IBalanceSource } from '@services/balances/types';
+import { BalanceService } from '@services/balances/balance-service';
+import { given } from '@test-utils/bdd';
+
+const OWNER = '0x1a00e1E311009E56e3b0B9Ed6F86f5Ce128a1C01';
+
+const DAI = '0x6b175474e89094c44da98b954eedeac495271d0f';
+const USDC = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48';
+const WETH = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
+
+const SUPPORT: Record<ChainId, BalanceQueriesSupport> = {
+  [Chains.ETHEREUM.chainId]: {
+    getBalancesForTokens: true,
+    getTokensHeldByAccount: false,
+  },
+};
+
+describe('Balance Service', () => {
+  let service: IBalanceService;
+  given(() => {
+    service = new BalanceService(SOURCE);
+  });
+
+  test('supportedQueries is calculated based on source', () => {
+    expect(service.supportedQueries()).to.eql(SUPPORT);
+  });
+
+  describe('getBalancesForTokens', () => {
+    test('Returned balances is as expected', async () => {
+      const balances = await service.getBalancesForTokens({
+        account: OWNER,
+        tokens: {
+          [Chains.ETHEREUM.chainId]: [DAI, USDC, WETH],
+        },
+      });
+      expect(balances).to.have.keys(Chains.ETHEREUM.chainId);
+      expect(balances[Chains.ETHEREUM.chainId]).to.have.keys([DAI, USDC, WETH]);
+      expect(balances[Chains.ETHEREUM.chainId][DAI]).to.equal('0');
+      expect(balances[Chains.ETHEREUM.chainId][USDC]).to.equal('10000');
+      expect(balances[Chains.ETHEREUM.chainId][WETH]).to.equal('20000');
+    });
+  });
+});
+
+const SOURCE: IBalanceSource = {
+  supportedQueries(): Record<ChainId, BalanceQueriesSupport> {
+    return SUPPORT;
+  },
+  getTokensHeldByAccounts(_: {
+    accounts: Record<ChainId, Address[]>;
+    context?: { timeout?: TimeString };
+  }): Promise<Record<ChainId, Record<Address, Record<TokenAddress, AmountOfToken>>>> {
+    throw new Error('Not implemented');
+  },
+  getBalancesForTokens({
+    tokens,
+  }: {
+    tokens: Record<ChainId, Record<Address, TokenAddress[]>>;
+    context?: { timeout?: TimeString };
+  }): Promise<Record<ChainId, Record<Address, Record<TokenAddress, AmountOfToken>>>> {
+    const result: Record<ChainId, Record<Address, Record<TokenAddress, AmountOfToken>>> = {};
+    for (const [chainIdString, record] of Object.entries(tokens)) {
+      const chainId = Number(chainIdString);
+      result[chainId] = {};
+      for (const [address, tokens] of Object.entries(record)) {
+        result[chainId][address] = {};
+        for (let i = 0; i < tokens.length; i++) {
+          result[chainId][address][tokens[i]] = `${i * 10000}`;
+        }
+      }
+    }
+    return Promise.resolve(result);
+  },
+};
