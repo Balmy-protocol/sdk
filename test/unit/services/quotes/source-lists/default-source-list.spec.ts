3135dee493f65ccc3fc81eed1c62c7ed06b2f9f9
Sam Bugs <sambugs@protonmail.com>
2023-03-21 09:18:31 -0300

    fix: fix unit test

--- test/unit/services/quotes/source-lists/default-source-list.spec.ts
+++ test/unit/services/quotes/source-lists/default-source-list.spec.ts
@@ -11,11 +11,17 @@ import { TransactionRequest } from '@ethersproject/providers';
 import { BigNumberish } from 'ethers';
 import { ChainId } from '@types';
 import chaiAsPromised from 'chai-as-promised';
+import { BaseTokenMetadata } from '@services/tokens/types';
+import { RPCTokenSource } from '@services/tokens/token-sources/rpc-token-source';
+import { MulticallService } from '@services/multicall/multicall-service';
+import { PriceService } from '@services/prices/price-service';
+import { DefiLlamaPriceSource } from '@services/prices/price-sources/defi-llama';
 chai.use(chaiAsPromised);
 
 const PROVIDER_SOURCE = new PublicRPCsSource();
 const FETCH_SERVICE = new FetchService(crossFetch);
-const TOKEN_SERVICE = new TokenService(new DefiLlamaTokenSource(FETCH_SERVICE));
+const TOKEN_SERVICE = new TokenService(new RPCTokenSource(new MulticallService(PROVIDER_SOURCE)));
+const PRICE_SERVICE = new PriceService(new DefiLlamaPriceSource(FETCH_SERVICE));
 const FAILING_GAS_SERVICE: IGasService<SupportedGasValues> = {
   supportedSpeeds: () => ({}),
   supportedChains: () => [1, 2, 3],
@@ -32,6 +38,7 @@ describe('Default Source List', () => {
       providerSource: PROVIDER_SOURCE,
       fetchService: FETCH_SERVICE,
       tokenService: TOKEN_SERVICE,
+      priceService: PRICE_SERVICE,
       gasService: FAILING_GAS_SERVICE,
       config: undefined,
     });
@@ -55,6 +62,7 @@ describe('Default Source List', () => {
       providerSource: PROVIDER_SOURCE,
       fetchService: FETCH_SERVICE,
       tokenService: TOKEN_SERVICE,
+      priceService: PRICE_SERVICE,
       gasService: FAILING_GAS_SERVICE,
       config: undefined,
     });
