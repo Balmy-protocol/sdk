8a82effe94d722738ba65ed57c703e45a6c9479d
Sam Bugs <sambugs@protonmail.com>
2023-04-09 20:38:09 -0300

    fix: integration tests

--- test/integration/services/quotes/sources/quote-sources.spec.ts
+++ test/integration/services/quotes/sources/quote-sources.spec.ts
@@ -287,9 +287,9 @@ describe('Quote Sources', () => {
         let slippage = SLIPPAGE_PERCENTAGE;
         if (ROUNDING_ISSUES.includes(sourceId)) slippage += 0.05;
         if (quote.type === 'sell') {
-          expect(quote.minBuyAmount).to.be.gte(substractPercentage(quote.buyAmount, slippage));
+          expect(quote.minBuyAmount).to.be.gte(substractPercentage(quote.buyAmount.toString(), slippage));
         } else {
-          expect(quote.maxSellAmount).to.be.lte(addPercentage(quote.sellAmount, slippage));
+          expect(quote.maxSellAmount).to.be.lte(addPercentage(quote.sellAmount.toString(), slippage));
         }
       }
 
