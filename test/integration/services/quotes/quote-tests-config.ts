5d933694cf5725cf2b550cc1688aa2bfd0ccb970
Sam Bugs <sambugs@protonmail.com>
2023-06-07 19:16:30 -0300

    test: use 0x api key when present

--- test/integration/services/quotes/quote-tests-config.ts
+++ test/integration/services/quotes/quote-tests-config.ts
@@ -18,6 +18,9 @@ if (process.env.FIREBIRD_API_KEY) {
 if (process.env.CHANGELLY_API_KEY) {
   CONFIG.custom!.changelly = { apiKey: process.env.CHANGELLY_API_KEY };
 }
+if (process.env.ZRX_API_KEY) {
+  CONFIG.custom!['0x'] = { apiKey: process.env.ZRX_API_KEY };
+}
 
 export function supportedChains() {
   const sources = QUOTE_SOURCES;
