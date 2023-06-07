27543d384133875312f77028bd0b08d2db2c0f3d
Sam Bugs <sambugs@protonmail.com>
2023-06-07 10:29:26 -0300

    refactor: expose base provider in provider service

--- src/services/providers/types.ts
+++ src/services/providers/types.ts
@@ -1,5 +1,5 @@
 import { ChainId } from '@types';
-import { Provider } from '@ethersproject/abstract-provider';
+import { BaseProvider } from '@ethersproject/providers';
 import { PublicClient, Transport } from 'viem';
 
 export type ProviderClientSupport = { ethers: boolean; viem: boolean };
@@ -7,12 +7,12 @@ export type ProviderClientSupport = { ethers: boolean; viem: boolean };
 export type IProviderService = {
   supportedChains(): ChainId[];
   supportedClients(): Record<ChainId, ProviderClientSupport>;
-  getEthersProvider(_: { chainId: ChainId }): Provider;
+  getEthersProvider(_: { chainId: ChainId }): BaseProvider;
   getViemPublicClient(_: { chainId: ChainId }): PublicClient;
 };
 
 export type IProviderSource = {
   supportedClients(): Record<ChainId, ProviderClientSupport>;
-  getEthersProvider(_: { chainId: ChainId }): Provider;
+  getEthersProvider(_: { chainId: ChainId }): BaseProvider;
   getViemTransport(_: { chainId: ChainId }): Transport;
 };
