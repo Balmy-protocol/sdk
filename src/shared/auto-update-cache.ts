d5f3ac38705f8b6d7b554033a030c8e0ffcd8b2c
koaj <koaj@balmy.xyz>
2023-09-05 10:59:57 -0300

    feat: default retry time, valid check

--- src/shared/auto-update-cache.ts
+++ src/shared/auto-update-cache.ts
@@ -1,9 +1,12 @@
+import { TimeString } from '@types';
 import ms, { StringValue } from 'ms';
 type Timestamp = number;
 type CacheConstructorParams<Value> = {
   calculate: () => Promise<Value>;
   config: AutoUpdateCacheConfig;
 };
+
+const DEFAULT_RETRY_TIME: TimeString = '5M';
 
 export type AutoUpdateCacheConfig = {
   valid?: 'always' | { onlyFor: StringValue };
@@ -20,6 +23,10 @@ export class AutoUpdateCache<Value> {
     this.config = config;
     this.cache = { lastUpdated: 0, value: undefined };
 
+    const isInvalid = config.valid && config.valid !== 'always' && !config.valid.onlyFor;
+
+    if (isInvalid) throw new Error(`'valid' property is misconfigured`);
+
     this.update();
   }
 
@@ -31,10 +38,10 @@ export class AutoUpdateCache<Value> {
         if (value !== undefined) {
           this.cache = { lastUpdated: Date.now(), value };
         }
-        setTimeout(this.update, this.config.update.every);
+        setTimeout(this.update, ms(this.config.update.every));
       })
       .catch(() => {
-        setTimeout(this.update, this.config.update.ifFailsTryAgainIn);
+        setTimeout(this.update, ms(this.config.update.ifFailsTryAgainIn ?? DEFAULT_RETRY_TIME));
       });
   }
 
@@ -42,7 +49,9 @@ export class AutoUpdateCache<Value> {
     const now = Date.now();
 
     const isValid = ({ lastUpdated }: { lastUpdated: Timestamp }) =>
-      this.config.valid === 'always' || lastUpdated >= now - ms(this.config.valid?.onlyFor);
+      !this.config.valid ||
+      this.config.valid === 'always' ||
+      (this.config.valid?.onlyFor && lastUpdated >= now - ms(this.config.valid?.onlyFor));
 
     return this.cache && isValid(this.cache) ? this.cache.value : undefined;
   }
