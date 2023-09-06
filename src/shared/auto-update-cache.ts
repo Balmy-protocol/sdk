3c735f2f040ab06c989f00dd82c77bdcf67989a5
koaj <koaj@balmy.xyz>
2023-09-06 14:23:23 -0300

    refactor: requested changes

--- src/shared/auto-update-cache.ts
+++ src/shared/auto-update-cache.ts
@@ -1,16 +1,15 @@
-import { TimeString } from '@types';
-import ms, { StringValue } from 'ms';
-type Timestamp = number;
+import { TimeString, Timestamp } from '@types';
+import ms from 'ms';
 type CacheConstructorParams<Value> = {
   calculate: () => Promise<Value>;
   config: AutoUpdateCacheConfig;
 };
 
-const DEFAULT_RETRY_TIME: TimeString = '5M';
+const DEFAULT_RETRY_TIME: TimeString = '5Minutes';
 
 export type AutoUpdateCacheConfig = {
-  valid?: 'always' | { onlyFor: StringValue };
-  update: { every: StringValue; ifFailsTryAgainIn?: StringValue };
+  valid?: 'always' | { onlyFor: TimeString };
+  update: { every: TimeString; ifFailsTryAgainIn?: TimeString };
 };
 
 export class AutoUpdateCache<Value> {
@@ -23,36 +22,31 @@ export class AutoUpdateCache<Value> {
     this.config = config;
     this.cache = { lastUpdated: 0, value: undefined };
 
-    const isInvalid = config.valid && config.valid !== 'always' && !config.valid.onlyFor;
+    const isInvalid = config.valid && config.valid !== 'always' && ms(config.valid.onlyFor) <= ms(config.update.every);
 
-    if (isInvalid) throw new Error(`'valid' property is misconfigured`);
+    if (isInvalid) throw new Error(`'onlyFor' must be greater than 'every'`);
 
     this.update();
   }
 
-  private async update() {
-    const calculated = this.calculate();
-    calculated
-      .then((result) => {
-        const value = result;
-        if (value !== undefined) {
-          this.cache = { lastUpdated: Date.now(), value };
-        }
-        setTimeout(() => this.update(), ms(this.config.update.every));
-      })
-      .catch(() => {
-        setTimeout(() => this.update(), ms(this.config.update.ifFailsTryAgainIn ?? DEFAULT_RETRY_TIME));
-      });
-  }
-
-  async getValue() {
+  getValue(): Value | undefined {
     const now = Date.now();
 
     const isValid = ({ lastUpdated }: { lastUpdated: Timestamp }) =>
-      !this.config.valid ||
-      this.config.valid === 'always' ||
-      (this.config.valid?.onlyFor && lastUpdated >= now - ms(this.config.valid?.onlyFor));
+      !this.config.valid || this.config.valid === 'always' || (this.config.valid.onlyFor && lastUpdated >= now - ms(this.config.valid.onlyFor));
 
     return this.cache && isValid(this.cache) ? this.cache.value : undefined;
+  }
+
+  private async update() {
+    try {
+      const result = await this.calculate();
+      if (result !== undefined) {
+        this.cache = { lastUpdated: Date.now(), value: result };
+      }
+      setTimeout(() => this.update(), ms(this.config.update.every));
+    } catch (error) {
+      setTimeout(() => this.update(), ms(this.config.update.ifFailsTryAgainIn ?? DEFAULT_RETRY_TIME));
+    }
   }
 }
