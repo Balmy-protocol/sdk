83037a9136841923517a45ece89fdb293e71b813
Sam Bugs <sambugs@protonmail.com>
2023-12-06 19:01:15 -0300

    feat: allow to build fastest balance service

--- src/sdk/builders/balance-builder.ts
+++ src/sdk/builders/balance-builder.ts
@@ -11,42 +11,43 @@ import { CachedBalanceSource } from '@services/balances/balance-sources/cached-b
 import { PortalsFiBalanceSource } from '@services/balances/balance-sources/portals-fi-balance-source';
 import { OneInchBalanceSource } from '@services/balances/balance-sources/1inch-balance-source';
 import { MagpieBalanceSource } from '@services/balances/balance-sources/magpie-balance-source';
+import { FastestBalanceSource } from '@services/balances/balance-sources/fastest-balance-source';
+import { BuildProviderParams, buildProviderService } from './provider-builder';
+import { buildMulticallService } from './multicall-builder';
 
 export type BalanceSourceInput =
-  | { type: 'rpc-multicall'; config?: RPCBalanceSourceConfig }
+  | { type: 'rpc-multicall'; config?: RPCBalanceSourceConfig; customProvider?: BuildProviderParams }
   | { type: 'cached'; underlyingSource: BalanceSourceInput; config: CacheConfig }
   | { type: 'custom'; instance: IBalanceSource }
   | { type: 'alchemy'; key: string }
   | { type: '1inch' }
   | { type: 'magpie' }
   | { type: 'portals-fi'; key: string }
-  | { type: 'moralis'; key: string };
+  | { type: 'moralis'; key: string }
+  | { type: 'fastest'; sources: BalanceSourceInput[] };
 export type BuildBalancesParams = { source: BalanceSourceInput };
 
 export function buildBalanceService(
   params: BuildBalancesParams | undefined,
   fetchService: IFetchService,
-  providerService: IProviderService,
-  multicallService: IMulticallService
+  providerService: IProviderService
 ): IBalanceService {
-  const source = buildSource(params?.source, { fetchService, providerService, multicallService });
+  const source = buildSource(params?.source, { fetchService, providerService });
   return new BalanceService(source);
 }
 
 function buildSource(
   source: BalanceSourceInput | undefined,
-  {
-    fetchService,
-    providerService,
-    multicallService,
-  }: { fetchService: IFetchService; providerService: IProviderService; multicallService: IMulticallService }
+  { fetchService, providerService }: { fetchService: IFetchService; providerService: IProviderService }
 ): IBalanceSource {
   switch (source?.type) {
     case undefined:
     case 'rpc-multicall':
-      return new RPCBalanceSource(providerService, multicallService, source?.config);
+      const provider = source?.customProvider ? buildProviderService(source.customProvider) : providerService;
+      const multicall = buildMulticallService(provider);
+      return new RPCBalanceSource(provider, multicall, source?.config);
     case 'cached':
-      const underlying = buildSource(source.underlyingSource, { fetchService, providerService, multicallService });
+      const underlying = buildSource(source.underlyingSource, { fetchService, providerService });
       return new CachedBalanceSource(underlying, source.config);
     case 'custom':
       return source.instance;
@@ -60,5 +61,7 @@ function buildSource(
       return new MagpieBalanceSource(fetchService);
     case 'moralis':
       return new MoralisBalanceSource(fetchService, source.key);
+    case 'fastest':
+      return new FastestBalanceSource(source.sources.map((source) => buildSource(source, { fetchService, providerService })));
   }
 }
