4a928e2b953a250f4192caa40fd324fe88684200
0xKoaj <koaj@balmy.xyz>
2024-04-12 11:10:03 -0300

    test: fixed

--- test/integration/services/gas/gas-price-sources.spec.ts
+++ test/integration/services/gas/gas-price-sources.spec.ts
@@ -15,7 +15,6 @@ import { FastestGasPriceSourceCombinator } from '@services/gas/gas-price-sources
 import { PolygonGasStationGasPriceSource } from '@services/gas/gas-price-sources/polygon-gas-station-gas-price-source';
 import { AggregatorGasPriceSource } from '@services/gas/gas-price-sources/aggregator-gas-price-source';
 import { ParaswapGasPriceSource } from '@services/gas/gas-price-sources/paraswap-gas-price-source';
-import { ChangellyGasPriceSource } from '@services/gas/gas-price-sources/changelly-gas-price-source';
 import { ProviderService } from '@services/providers/provider-service';
 import { CHAINS_WITH_KNOWN_ISSUES } from '@test-utils/other';
 import { LogsService } from '@services/logs/logs-service';
@@ -32,7 +31,6 @@ const FASTEST_GAS_SOURCE = new FastestGasPriceSourceCombinator([PARASWAP_SOURCE,
 const AGGREGATOR_GAS_SOURCE = new AggregatorGasPriceSource(LOGS_SERVICE, [PARASWAP_SOURCE, RPC_SOURCE], 'median');
 const ETH_GAS_STATION_SOURCE = new EthGasStationGasPriceSource(FETCH_SERVICE);
 const OWLRACLE_SOURCE = new OwlracleGasPriceSource(FETCH_SERVICE, '7d7859c452d5419bae3d7666c8130c96');
-const CHANGELLY_GAS_SOURCE = new ChangellyGasPriceSource(FETCH_SERVICE, process.env.CHANGELLY_API_KEY!);
 
 jest.retryTimes(3);
 jest.setTimeout(ms('30s'));
@@ -48,7 +46,6 @@ describe('Gas Price Sources', () => {
   gasPriceSourceTest({ title: 'Etherscan Source', source: ETHERSCAN_SOURCE });
   gasPriceSourceTest({ title: 'Aggregator Source', source: AGGREGATOR_GAS_SOURCE });
   gasPriceSourceTest({ title: 'Paraswap Source', source: PARASWAP_SOURCE });
-  // gasPriceSourceTest({ title: 'Changelly Source', source: CHANGELLY_GAS_SOURCE }); We comment this out because we need an API key
 
   function gasPriceSourceTest({ title, source }: { title: string; source: IGasPriceSource<object> }) {
     describe(title, () => {
