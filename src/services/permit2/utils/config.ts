cc6d6c1686bc34f229bdc3d9227f58fb92811b90
0xKoaj <koaj@balmy.xyz>
2024-04-22 17:42:13 -0300

    feat: update permit2adapter for polygon zkevm

--- src/services/permit2/utils/config.ts
+++ src/services/permit2/utils/config.ts
@@ -1,15 +1,8 @@
 import { Chains } from '@chains';
-import { ChainId } from '@types';
+import { Address, ChainId } from '@types';
 
 export const PERMIT2_ADDRESS = '0x000000000022d473030f116ddee9f6b43ac78ba3';
-export function PERMIT2_ADAPTER_ADDRESS(chainId: ChainId) {
-  switch (chainId) {
-    case Chains.POLYGON_ZKEVM.chainId:
-      return '0xA70C8401C058B6198e1cb085091DE13498CEc0dC';
-    default:
-      return '0xED306e38BB930ec9646FF3D917B2e513a97530b1';
-  }
-}
+export const PERMIT2_ADAPTER_ADDRESS = (_: ChainId): Address => '0xED306e38BB930ec9646FF3D917B2e513a97530b1';
 
 export const WORDS_FOR_NONCE_CALCULATION = 10;
 export const PERMIT2_SUPPORTED_CHAINS = [
