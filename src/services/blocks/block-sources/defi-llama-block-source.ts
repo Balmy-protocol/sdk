b7f168a4f730cd00fb4b312662ff21764e190147
th0rOdinson <th0r@balmy.xyz>
2024-05-14 14:20:22 -0300

    feat: handle timestamp before genesis block

--- src/services/blocks/block-sources/defi-llama-block-source.ts
+++ src/services/blocks/block-sources/defi-llama-block-source.ts
@@ -2,12 +2,15 @@ import { ChainId, Timestamp } from '@types';
 import { IFetchService } from '@services/fetch';
 import { DefiLlamaClient } from '@shared/defi-llama';
 import { BlockResult, IBlocksSource } from '../types';
+import { IProviderService } from '@services/providers';
 
 export class DefiLlamaBlockSource implements IBlocksSource {
   private readonly defiLlama: DefiLlamaClient;
+  private readonly providerService: IProviderService;
 
-  constructor(fetch: IFetchService) {
+  constructor(fetch: IFetchService, providerService: IProviderService) {
     this.defiLlama = new DefiLlamaClient(fetch);
+    this.providerService = providerService;
   }
 
   supportedChains(): ChainId[] {
@@ -23,7 +26,21 @@ export class DefiLlamaBlockSource implements IBlocksSource {
     const promises: Promise<any>[] = [];
     for (const { chainId, timestamp } of timestamps) {
       if (!(chainId in result)) result[chainId] = {};
-      const promise = this.defiLlama.getClosestBlock(chainId, timestamp).then((block) => (result[chainId][timestamp] = block));
+      const promise = this.defiLlama
+        .getClosestBlock(chainId, timestamp)
+        .then((block) => (result[chainId][timestamp] = block))
+        .catch(async (e) => {
+          const provider = this.providerService.getViemPublicClient({ chainId });
+
+          // We're getting a timestamp value of 0n for genesis block, so we will use block 1n for now
+          const blockOne = await provider.getBlock({ blockNumber: 1n });
+          const blockTimestamp = Number(blockOne.timestamp);
+          if (timestamp < blockTimestamp) {
+            result[chainId][timestamp] = { block: 1n, timestamp: blockTimestamp };
+          } else {
+            throw e;
+          }
+        });
       promises.push(promise);
     }
     await Promise.all(promises);
