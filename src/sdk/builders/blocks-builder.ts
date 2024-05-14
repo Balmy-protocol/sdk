c3d1ce4ad2e5b41767d86301c45a7445c88f8ef8
th0r <144699868+th0rOdinson@users.noreply.github.com>
2024-05-14 15:15:24 -0300

    feat: handle timestamp before genesis block (#503)

--- src/sdk/builders/blocks-builder.ts
+++ src/sdk/builders/blocks-builder.ts
@@ -2,20 +2,28 @@ import { IFetchService } from '@services/fetch';
 import { IBlocksService, IBlocksSource } from '@services/blocks';
 import { DefiLlamaBlockSource } from '@services/blocks/block-sources/defi-llama-block-source';
 import { BlocksService } from '@services/blocks/block-service';
+import { IProviderService } from '@services/providers';
 
 export type BlocksSourceInput = { type: 'custom'; instance: IBlocksSource } | { type: 'defi-llama' };
 export type BuildBlocksParams = { source: BlocksSourceInput };
 
-export function buildBlocksService(params: BuildBlocksParams | undefined, fetchService: IFetchService): IBlocksService {
-  const source = buildSource(params?.source, { fetchService });
+export function buildBlocksService(
+  params: BuildBlocksParams | undefined,
+  fetchService: IFetchService,
+  providerService: IProviderService
+): IBlocksService {
+  const source = buildSource(params?.source, { fetchService, providerService });
   return new BlocksService(source);
 }
 
-function buildSource(source: BlocksSourceInput | undefined, { fetchService }: { fetchService: IFetchService }): IBlocksSource {
+function buildSource(
+  source: BlocksSourceInput | undefined,
+  { fetchService, providerService }: { fetchService: IFetchService; providerService: IProviderService }
+): IBlocksSource {
   switch (source?.type) {
     case undefined:
     case 'defi-llama':
-      return new DefiLlamaBlockSource(fetchService);
+      return new DefiLlamaBlockSource(fetchService, providerService);
     case 'custom':
       return source.instance;
   }
