ed212b4ec4fb104965d983f626e7146aa0e04b98
0xKoaj <141654454+0xKoaj@users.noreply.github.com>
2024-09-24 09:38:01 -0300

    feat: add buildClaimDelayedWithdrawPositionTx to earn-service (#582)
    * feat: add buildClaimDelayedWithdrawPositionTx to earn-service
    
    * feat: multiple tokens to claim
    
    * fix: fix condition for token conversion in EarnService
    
    * refactor: optimize token conversion logic in EarnService
    
    * chore: fix lint
    
    * refactor: some suggestions
    
    * refactor: optimize token conversion logic in EarnService
    
    * refactor: optimize token conversion logic in EarnService
    
    * fix: claim tokens variable
    
    * refactor: infer type from multicall

---
+++ src/shared/abis/earn-delayed-withdrawal-manager.ts
@@ -0,0 +1,79 @@
+export default [
+  {
+    type: 'function',
+    name: 'withdrawableFunds',
+    inputs: [
+      {
+        name: 'positionId',
+        type: 'uint256',
+        internalType: 'uint256',
+      },
+      {
+        name: 'token',
+        type: 'address',
+        internalType: 'address',
+      },
+    ],
+    outputs: [
+      {
+        name: 'funds',
+        type: 'uint256',
+        internalType: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+  },
+  {
+    type: 'function',
+    name: 'withdraw',
+    inputs: [
+      {
+        name: 'positionId',
+        type: 'uint256',
+        internalType: 'uint256',
+      },
+      {
+        name: 'token',
+        type: 'address',
+        internalType: 'address',
+      },
+      {
+        name: 'recipient',
+        type: 'address',
+        internalType: 'address',
+      },
+    ],
+    outputs: [
+      {
+        name: 'withdrawn',
+        type: 'uint256',
+        internalType: 'uint256',
+      },
+      {
+        name: 'stillPending',
+        type: 'uint256',
+        internalType: 'uint256',
+      },
+    ],
+    stateMutability: 'nonpayable',
+  },
+  {
+    type: 'function',
+    name: 'multicall',
+    inputs: [
+      {
+        name: 'data',
+        type: 'bytes[]',
+        internalType: 'bytes[]',
+      },
+    ],
+    outputs: [
+      {
+        name: 'results',
+        type: 'bytes[]',
+        internalType: 'bytes[]',
+      },
+    ],
+    stateMutability: 'payable',
+  },
+] as const;
