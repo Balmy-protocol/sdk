a30ffd8d77905a65e5972f7e1b68227ab7cfe919
0xKoaj <141654454+0xKoaj@users.noreply.github.com>
2024-11-08 10:49:30 -0300

    feat: add new earn addresses for polygon and base (#608)

--- src/services/earn/config.ts
+++ src/services/earn/config.ts
@@ -1,12 +1,21 @@
+import { Chains } from '@chains';
 import { PERMIT2_ADAPTER_CONTRACT } from '@services/permit2/utils/config';
 import { Contract } from '@shared/contracts';
 
-export const EARN_VAULT = Contract.with({ defaultAddress: '0x63a8aE714568EC8f8Ec14472674c68582b0B0458' }).build();
+export const EARN_VAULT = Contract.with({ defaultAddress: '0x58e5d76fbbd7e1b51f0fc0f66b7734e108be0461' }) // Polygon
+  .and({ address: '0x9324a1f92a82b539f8fb1194a0b894025581ec33', onChain: Chains.BASE })
+  .build();
 
-export const EARN_VAULT_COMPANION = Contract.with({ defaultAddress: '0xDc6C90877AF3a457e6e5E1C12fCe6Ad9DdC3D136' }).build();
+export const EARN_VAULT_COMPANION = Contract.with({ defaultAddress: '0x814CE82aCDE3D99aB6f92e2722A87d8daa20d6cB' }) // Polygon
+  .and({ address: '0x260B192b9A5679121FF7f9F0Cf3FED3238753A48', onChain: Chains.BASE })
+  .build();
 
-export const EARN_STRATEGY_REGISTRY = Contract.with({ defaultAddress: '0xF04647173Cd720158a2D2e5667343F7c397265cA' }).build();
+export const EARN_STRATEGY_REGISTRY = Contract.with({ defaultAddress: '0xb034a43d1ffe0f88ed3a50fc096179f543fd3f3a' }) // Polygon
+  .and({ address: '0x04a3bd5cc16435f27e7b31e44e4e522c0bc413e9', onChain: Chains.BASE })
+  .build();
 
 export const COMPANION_SWAPPER_CONTRACT = PERMIT2_ADAPTER_CONTRACT;
 
-export const DELAYED_WITHDRAWAL_MANAGER = Contract.with({ defaultAddress: '0xA4576169267E89DB08bA9eCef67dc6A2F8046568' }).build();
+export const DELAYED_WITHDRAWAL_MANAGER = Contract.with({ defaultAddress: '0x92fdb7604ca7e5635421332d4c9a00680dfa53e6' }) // Polygon
+  .and({ address: '0x9b5004c016fe00bb81c90e2e2beaa128d1815108', onChain: Chains.BASE })
+  .build();
